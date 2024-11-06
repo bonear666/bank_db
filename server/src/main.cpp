53cc63aaf9292061a150c954dffdd6bf890a0b57
Bonear <noemail>
2024-11-06 22:12:39 +0500

    -

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -6,16 +6,31 @@
 //#include <Windows.h>
 #include <microhttpd.h>
 
-#define STD_C_ERROR -1
+#define C_STD_ERROR -1
+#define HOST_NAME_OFFSET (sizeof("http://") - 1)
 
-#define PORT          80
-#define FILE_NAME     "/res/cat.jpg"
-#define MIME_TYPE     "image/jpg"
-#define USER_NAME     ""
+#define PORT 80
+#define FILE_NAME "/res/cat.jpg"
+#define HOME_PAGE_NAME "/res/home_page.html"
+#define MIME_TYPE "image/jpg"
+#define MIME_HTML "text/html; charset=UTF-8"
+#define USER_NAME ""
 #define USER_PASSWORD ""
 #define REQUEST_BUFFER_LENGTH 150
+#define HOST_NAME "sigma-bank.ru"
+#define HOST_NAME_LENGTH 14 //null-terminated 
 
 char* db_request_buffer;
+const char* const page_names[] = 
+{
+	"sigma-bank.ru",
+	"sigma-bank.ru/employee",
+	"sigma-bank.ru/client"
+};
+const char* const resource_names[] = 
+{
+	"/res/home_img1.jpg"
+};
 
 MHD_Result Print_Out_Key(
 	void* cls, enum MHD_ValueKind kind, 
@@ -63,12 +78,9 @@ int DB_Auth_Request(char member_type, const char* username, const char* password
 	if (*request_result != NULL)
 	{
 		if (PQntuples(*request_result) != 0) {return EXIT_SUCCESS;}
-		else 								 {return EXIT_FAILURE;}
 	} 
-	else
-	{
-		return EXIT_FAILURE;
-	}
+
+	return EXIT_FAILURE;
 };
 
 MHD_Result Answer_To_Connection(
@@ -92,6 +104,41 @@ MHD_Result Answer_To_Connection(
 	//handling GET-request
 	if (strcmp("GET", method) != 0) return MHD_NO;
 	if (*con_cls == NULL) {*con_cls = connection; return MHD_YES;}
+	
+	//url exploration (http, no secure)
+	//home page has been requsted
+	if 		(strcmp(url + HOST_NAME_OFFSET, page_names[0]) == 0)
+	{
+		if (((response_file = fopen(HOME_PAGE_NAME, "r")) == NULL) ||
+			 (fstat(_fileno(response_file), &file_stat)   == C_STD_ERROR))
+			 {
+				 Iternal_Error_Handling();
+				 
+				 return MHD_NO;
+			 }
+		
+		response = MHD_create_response_from_fd_at_offset64(file_stat.st_size, _fileno(response_file), 0);
+		MHD_add_response_header(response, "Content-Type", MIME_HTML);
+		ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+		MHD_destroy_response(response);
+		
+		return ret;
+	}
+	//employee page has been requested
+	else if (strcmp(url + HOST_NAME_OFFSET, page_names[1]) == 0)
+	{
+		
+	}
+	//clinet page has been requested
+	else if (strcmp(url + HOST_NAME_OFFSET, page_names[2]) == 0)
+	{
+		
+	}
+	//page resource has been requested
+	else if (strcmp(url, "/res/") > 0)
+	{
+		
+	}
 	
 	//handling authentication
 	password = NULL;
@@ -120,7 +167,7 @@ MHD_Result Answer_To_Connection(
 	
 	//response file errors handling
 	if (((response_file = fopen(FILE_NAME, "r"))	== NULL) ||
-		 (fstat(_fileno(response_file), &file_stat) == STD_C_ERROR))
+		 (fstat(_fileno(response_file), &file_stat) == C_STD_ERROR))
 	{
 		const char* error_str = "<html><body>An internal server error has occurred!</body></html>";
 
